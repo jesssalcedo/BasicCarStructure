190a717cd90cb39713068ca1ef1851d1e556e2b3
Jesssalcedo <jess.salcedo92@gmail.com>
2023-03-29 19:21:00 -0400

    commitcar

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+
+    public static void main(String[] args) {
+
+            BasicStructure car = new BasicStructure();
+            car.model = "A3";
+            car.power = "5000";
+            car.color = "Black";
+
+        }
+    }
